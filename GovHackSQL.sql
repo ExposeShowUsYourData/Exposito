847645e8956d4ead524148230f7c63aef11a4549
nguny009 <55015450+nguny009@users.noreply.github.com>
2019-09-08 13:41:43 +0930

    Update GovHackSQL.sql

--- GovHackSQL.sql
+++ GovHackSQL.sql
@@ -1,60 +1,7 @@
-/****** Object:  Schema [LDA]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Schema [LDA]    Script Date: 8/09/2019 1:41:29 PM ******/
 CREATE SCHEMA [LDA]
 GO
-/****** Object:  UserDefinedFunction [dbo].[fn_diagramobjects]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE FUNCTION [dbo].[fn_diagramobjects]() 
-	RETURNS int
-	WITH EXECUTE AS N'dbo'
-	AS
-	BEGIN
-		declare @id_upgraddiagrams		int
-		declare @id_sysdiagrams			int
-		declare @id_helpdiagrams		int
-		declare @id_helpdiagramdefinition	int
-		declare @id_creatediagram	int
-		declare @id_renamediagram	int
-		declare @id_alterdiagram 	int 
-		declare @id_dropdiagram		int
-		declare @InstalledObjects	int
-
-		select @InstalledObjects = 0
-
-		select 	@id_upgraddiagrams = object_id(N'dbo.sp_upgraddiagrams'),
-			@id_sysdiagrams = object_id(N'dbo.sysdiagrams'),
-			@id_helpdiagrams = object_id(N'dbo.sp_helpdiagrams'),
-			@id_helpdiagramdefinition = object_id(N'dbo.sp_helpdiagramdefinition'),
-			@id_creatediagram = object_id(N'dbo.sp_creatediagram'),
-			@id_renamediagram = object_id(N'dbo.sp_renamediagram'),
-			@id_alterdiagram = object_id(N'dbo.sp_alterdiagram'), 
-			@id_dropdiagram = object_id(N'dbo.sp_dropdiagram')
-
-		if @id_upgraddiagrams is not null
-			select @InstalledObjects = @InstalledObjects + 1
-		if @id_sysdiagrams is not null
-			select @InstalledObjects = @InstalledObjects + 2
-		if @id_helpdiagrams is not null
-			select @InstalledObjects = @InstalledObjects + 4
-		if @id_helpdiagramdefinition is not null
-			select @InstalledObjects = @InstalledObjects + 8
-		if @id_creatediagram is not null
-			select @InstalledObjects = @InstalledObjects + 16
-		if @id_renamediagram is not null
-			select @InstalledObjects = @InstalledObjects + 32
-		if @id_alterdiagram  is not null
-			select @InstalledObjects = @InstalledObjects + 64
-		if @id_dropdiagram is not null
-			select @InstalledObjects = @InstalledObjects + 128
-		
-		return @InstalledObjects 
-	END
-	
-GO
-/****** Object:  Table [dbo].[census_ASGS_LGA]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_ASGS_LGA]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -64,7 +11,7 @@ CREATE TABLE [dbo].[census_ASGS_LGA](
 	[Areadesc] [nvarchar](200) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  View [dbo].[vw_census_ASGS_LGA]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  View [dbo].[vw_census_ASGS_LGA]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -72,7 +19,7 @@ GO
 create view [dbo].[vw_census_ASGS_LGA]
 as select distinct code , Areadesc from [dbo].[census_ASGS_LGA]
 GO
-/****** Object:  Table [dbo].[census_ASGS]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_ASGS]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -83,7 +30,7 @@ CREATE TABLE [dbo].[census_ASGS](
 	[ASGSCode] [nvarchar](20) NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_ASGS_POA]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_ASGS_POA]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -93,7 +40,7 @@ CREATE TABLE [dbo].[census_ASGS_POA](
 	[Areadesc] [nvarchar](200) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_ASGS_SA1]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_ASGS_SA1]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -103,7 +50,7 @@ CREATE TABLE [dbo].[census_ASGS_SA1](
 	[Areadesc] [nvarchar](200) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_ASGS_SSC]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_ASGS_SSC]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -113,7 +60,7 @@ CREATE TABLE [dbo].[census_ASGS_SSC](
 	[Areadesc] [nvarchar](200) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_data]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_data]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -129,7 +76,7 @@ CREATE TABLE [dbo].[census_data](
 	[AdfSliceIdentifier] [binary](32) NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_metadata]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_metadata]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -140,7 +87,7 @@ CREATE TABLE [dbo].[census_metadata](
 	[tablecode] [nvarchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_table]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_table]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -150,7 +97,7 @@ CREATE TABLE [dbo].[census_table](
 	[tabledesc] [nvarchar](300) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[census_table_test]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [dbo].[census_table_test]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -160,38 +107,7 @@ CREATE TABLE [dbo].[census_table_test](
 	[tabledesc] [nvarchar](300) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[sysdiagrams]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[sysdiagrams](
-	[name] [sysname] NOT NULL,
-	[principal_id] [int] NOT NULL,
-	[diagram_id] [int] IDENTITY(1,1) NOT NULL,
-	[version] [int] NULL,
-	[definition] [varbinary](max) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[diagram_id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [UK_principal_name] UNIQUE NONCLUSTERED 
-(
-	[principal_id] ASC,
-	[name] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[test1]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[test1](
-	[col1] [varchar](50) NULL
-) ON [PRIMARY]
-GO
-/****** Object:  Table [LDA].[OverseasArrivalsAndDepartures]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [LDA].[OverseasArrivalsAndDepartures]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -215,7 +131,7 @@ CREATE TABLE [LDA].[OverseasArrivalsAndDepartures](
 	[Jun] [float] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [LDA].[stocktake-of-australian-innovation-precincts-csv-july-2019-qa]    Script Date: 8/09/2019 1:39:30 PM ******/
+/****** Object:  Table [LDA].[stocktake-of-australian-innovation-precincts-csv-july-2019-qa]    Script Date: 8/09/2019 1:41:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -233,441 +149,4 @@ CREATE TABLE [LDA].[stocktake-of-australian-innovation-precincts-csv-july-2019-q
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[census_data] ADD  DEFAULT (getdate()) FOR [loadedtime]
-GO
-/****** Object:  StoredProcedure [dbo].[pop_census_asgs]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-create procedure [dbo].[pop_census_asgs]
-	@typeasgs nvarchar(50)
-as
-	set nocount on;
-	if @typeasgs = 'LGA'  
-		delete from census_ASGS where [ASGSCode] = 'LGA';
-		insert into census_ASGS([code],[Areadesc],[ASGSCode])
-		select 'LGA'+ [code], [Areadesc] , 'LGA' from [dbo].[census_ASGS_LGA];
-	if @typeasgs = 'POA'  
-		delete from census_ASGS where [ASGSCode] = 'POA';
-		insert into census_ASGS([code],[Areadesc],[ASGSCode])
-		select distinct 'POA'+ [code], [Areadesc] , 'POA' from [dbo].[census_ASGS_POA];
-	if @typeasgs = 'SA1'  
-		delete from census_ASGS where [ASGSCode] = 'SA1';
-		insert into census_ASGS([code],[Areadesc],[ASGSCode])
-		select [code], [Areadesc] , 'SA1' from [dbo].[census_ASGS_SA1];
-	if @typeasgs = 'SSC'  
-		delete from census_ASGS where [ASGSCode] = 'SSC';
-		insert into census_ASGS([code],[Areadesc],[ASGSCode])
-		select 'SSC'+ [code], [Areadesc] , 'SSC' from [dbo].[census_ASGS_SSC];
-	if @typeasgs = 'ALL'  
-		truncate table census_ASGS ;
-		insert into census_ASGS([code],[Areadesc],[ASGSCode])
-		select 'LGA'+[code], [Areadesc] , 'LGA' from [dbo].[census_ASGS_LGA]
-		union all
-		select 'SSC' + [code], [Areadesc] , 'SSC' from [dbo].[census_ASGS_SSC]
-		union all 
-		select distinct 'POA'+ [code], [Areadesc] , 'POA' from [dbo].[census_ASGS_POA]
-		union all
-		select [code], [Areadesc] , 'SA1' from [dbo].[census_ASGS_SA1];
-
-
-GO
-/****** Object:  StoredProcedure [dbo].[sp_alterdiagram]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_alterdiagram]
-	(
-		@diagramname 	sysname,
-		@owner_id	int	= null,
-		@version 	int,
-		@definition 	varbinary(max)
-	)
-	WITH EXECUTE AS 'dbo'
-	AS
-	BEGIN
-		set nocount on
-	
-		declare @theId 			int
-		declare @retval 		int
-		declare @IsDbo 			int
-		
-		declare @UIDFound 		int
-		declare @DiagId			int
-		declare @ShouldChangeUID	int
-	
-		if(@diagramname is null)
-		begin
-			RAISERROR ('Invalid ARG', 16, 1)
-			return -1
-		end
-	
-		execute as caller;
-		select @theId = DATABASE_PRINCIPAL_ID();	 
-		select @IsDbo = IS_MEMBER(N'db_owner'); 
-		if(@owner_id is null)
-			select @owner_id = @theId;
-		revert;
-	
-		select @ShouldChangeUID = 0
-		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
-		
-		if(@DiagId IS NULL or (@IsDbo = 0 and @theId <> @UIDFound))
-		begin
-			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1);
-			return -3
-		end
-	
-		if(@IsDbo <> 0)
-		begin
-			if(@UIDFound is null or USER_NAME(@UIDFound) is null) -- invalid principal_id
-			begin
-				select @ShouldChangeUID = 1 ;
-			end
-		end
-
-		-- update dds data			
-		update dbo.sysdiagrams set definition = @definition where diagram_id = @DiagId ;
-
-		-- change owner
-		if(@ShouldChangeUID = 1)
-			update dbo.sysdiagrams set principal_id = @theId where diagram_id = @DiagId ;
-
-		-- update dds version
-		if(@version is not null)
-			update dbo.sysdiagrams set version = @version where diagram_id = @DiagId ;
-
-		return 0
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_creatediagram]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_creatediagram]
-	(
-		@diagramname 	sysname,
-		@owner_id		int	= null, 	
-		@version 		int,
-		@definition 	varbinary(max)
-	)
-	WITH EXECUTE AS 'dbo'
-	AS
-	BEGIN
-		set nocount on
-	
-		declare @theId int
-		declare @retval int
-		declare @IsDbo	int
-		declare @userName sysname
-		if(@version is null or @diagramname is null)
-		begin
-			RAISERROR (N'E_INVALIDARG', 16, 1);
-			return -1
-		end
-	
-		execute as caller;
-		select @theId = DATABASE_PRINCIPAL_ID(); 
-		select @IsDbo = IS_MEMBER(N'db_owner');
-		revert; 
-		
-		if @owner_id is null
-		begin
-			select @owner_id = @theId;
-		end
-		else
-		begin
-			if @theId <> @owner_id
-			begin
-				if @IsDbo = 0
-				begin
-					RAISERROR (N'E_INVALIDARG', 16, 1);
-					return -1
-				end
-				select @theId = @owner_id
-			end
-		end
-		-- next 2 line only for test, will be removed after define name unique
-		if EXISTS(select diagram_id from dbo.sysdiagrams where principal_id = @theId and name = @diagramname)
-		begin
-			RAISERROR ('The name is already used.', 16, 1);
-			return -2
-		end
-	
-		insert into dbo.sysdiagrams(name, principal_id , version, definition)
-				VALUES(@diagramname, @theId, @version, @definition) ;
-		
-		select @retval = @@IDENTITY 
-		return @retval
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_dropdiagram]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_dropdiagram]
-	(
-		@diagramname 	sysname,
-		@owner_id	int	= null
-	)
-	WITH EXECUTE AS 'dbo'
-	AS
-	BEGIN
-		set nocount on
-		declare @theId 			int
-		declare @IsDbo 			int
-		
-		declare @UIDFound 		int
-		declare @DiagId			int
-	
-		if(@diagramname is null)
-		begin
-			RAISERROR ('Invalid value', 16, 1);
-			return -1
-		end
-	
-		EXECUTE AS CALLER;
-		select @theId = DATABASE_PRINCIPAL_ID();
-		select @IsDbo = IS_MEMBER(N'db_owner'); 
-		if(@owner_id is null)
-			select @owner_id = @theId;
-		REVERT; 
-		
-		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
-		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId))
-		begin
-			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1)
-			return -3
-		end
-	
-		delete from dbo.sysdiagrams where diagram_id = @DiagId;
-	
-		return 0;
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_helpdiagramdefinition]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_helpdiagramdefinition]
-	(
-		@diagramname 	sysname,
-		@owner_id	int	= null 		
-	)
-	WITH EXECUTE AS N'dbo'
-	AS
-	BEGIN
-		set nocount on
-
-		declare @theId 		int
-		declare @IsDbo 		int
-		declare @DiagId		int
-		declare @UIDFound	int
-	
-		if(@diagramname is null)
-		begin
-			RAISERROR (N'E_INVALIDARG', 16, 1);
-			return -1
-		end
-	
-		execute as caller;
-		select @theId = DATABASE_PRINCIPAL_ID();
-		select @IsDbo = IS_MEMBER(N'db_owner');
-		if(@owner_id is null)
-			select @owner_id = @theId;
-		revert; 
-	
-		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname;
-		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId ))
-		begin
-			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1);
-			return -3
-		end
-
-		select version, definition FROM dbo.sysdiagrams where diagram_id = @DiagId ; 
-		return 0
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_helpdiagrams]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_helpdiagrams]
-	(
-		@diagramname sysname = NULL,
-		@owner_id int = NULL
-	)
-	WITH EXECUTE AS N'dbo'
-	AS
-	BEGIN
-		DECLARE @user sysname
-		DECLARE @dboLogin bit
-		EXECUTE AS CALLER;
-			SET @user = USER_NAME();
-			SET @dboLogin = CONVERT(bit,IS_MEMBER('db_owner'));
-		REVERT;
-		SELECT
-			[Database] = DB_NAME(),
-			[Name] = name,
-			[ID] = diagram_id,
-			[Owner] = USER_NAME(principal_id),
-			[OwnerID] = principal_id
-		FROM
-			sysdiagrams
-		WHERE
-			(@dboLogin = 1 OR USER_NAME(principal_id) = @user) AND
-			(@diagramname IS NULL OR name = @diagramname) AND
-			(@owner_id IS NULL OR principal_id = @owner_id)
-		ORDER BY
-			4, 5, 1
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_renamediagram]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_renamediagram]
-	(
-		@diagramname 		sysname,
-		@owner_id		int	= null,
-		@new_diagramname	sysname
-	
-	)
-	WITH EXECUTE AS 'dbo'
-	AS
-	BEGIN
-		set nocount on
-		declare @theId 			int
-		declare @IsDbo 			int
-		
-		declare @UIDFound 		int
-		declare @DiagId			int
-		declare @DiagIdTarg		int
-		declare @u_name			sysname
-		if((@diagramname is null) or (@new_diagramname is null))
-		begin
-			RAISERROR ('Invalid value', 16, 1);
-			return -1
-		end
-	
-		EXECUTE AS CALLER;
-		select @theId = DATABASE_PRINCIPAL_ID();
-		select @IsDbo = IS_MEMBER(N'db_owner'); 
-		if(@owner_id is null)
-			select @owner_id = @theId;
-		REVERT;
-	
-		select @u_name = USER_NAME(@owner_id)
-	
-		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
-		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId))
-		begin
-			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1)
-			return -3
-		end
-	
-		-- if((@u_name is not null) and (@new_diagramname = @diagramname))	-- nothing will change
-		--	return 0;
-	
-		if(@u_name is null)
-			select @DiagIdTarg = diagram_id from dbo.sysdiagrams where principal_id = @theId and name = @new_diagramname
-		else
-			select @DiagIdTarg = diagram_id from dbo.sysdiagrams where principal_id = @owner_id and name = @new_diagramname
-	
-		if((@DiagIdTarg is not null) and  @DiagId <> @DiagIdTarg)
-		begin
-			RAISERROR ('The name is already used.', 16, 1);
-			return -2
-		end		
-	
-		if(@u_name is null)
-			update dbo.sysdiagrams set [name] = @new_diagramname, principal_id = @theId where diagram_id = @DiagId
-		else
-			update dbo.sysdiagrams set [name] = @new_diagramname where diagram_id = @DiagId
-		return 0
-	END
-	
-GO
-/****** Object:  StoredProcedure [dbo].[sp_upgraddiagrams]    Script Date: 8/09/2019 1:39:30 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-	CREATE PROCEDURE [dbo].[sp_upgraddiagrams]
-	AS
-	BEGIN
-		IF OBJECT_ID(N'dbo.sysdiagrams') IS NOT NULL
-			return 0;
-	
-		CREATE TABLE dbo.sysdiagrams
-		(
-			name sysname NOT NULL,
-			principal_id int NOT NULL,	-- we may change it to varbinary(85)
-			diagram_id int PRIMARY KEY IDENTITY,
-			version int,
-	
-			definition varbinary(max)
-			CONSTRAINT UK_principal_name UNIQUE
-			(
-				principal_id,
-				name
-			)
-		);
-
-
-		/* Add this if we need to have some form of extended properties for diagrams */
-		/*
-		IF OBJECT_ID(N'dbo.sysdiagram_properties') IS NULL
-		BEGIN
-			CREATE TABLE dbo.sysdiagram_properties
-			(
-				diagram_id int,
-				name sysname,
-				value varbinary(max) NOT NULL
-			)
-		END
-		*/
-
-		IF OBJECT_ID(N'dbo.dtproperties') IS NOT NULL
-		begin
-			insert into dbo.sysdiagrams
-			(
-				[name],
-				[principal_id],
-				[version],
-				[definition]
-			)
-			select	 
-				convert(sysname, dgnm.[uvalue]),
-				DATABASE_PRINCIPAL_ID(N'dbo'),			-- will change to the sid of sa
-				0,							-- zero for old format, dgdef.[version],
-				dgdef.[lvalue]
-			from dbo.[dtproperties] dgnm
-				inner join dbo.[dtproperties] dggd on dggd.[property] = 'DtgSchemaGUID' and dggd.[objectid] = dgnm.[objectid]	
-				inner join dbo.[dtproperties] dgdef on dgdef.[property] = 'DtgSchemaDATA' and dgdef.[objectid] = dgnm.[objectid]
-				
-			where dgnm.[property] = 'DtgSchemaNAME' and dggd.[uvalue] like N'_EA3E6268-D998-11CE-9454-00AA00A3F36E_' 
-			return 2;
-		end
-		return 1;
-	END
-	
 GO
